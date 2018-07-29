19f00eef7b97e7231accf29df55d8d92b5a0bf2b
shengwenxiong <123456>
2018-07-29 16:28:34 +0800

    commit BActivity

---
+++ src/main/java/com/xdx/textviewdemo/utils/TestBActivity.java
@@ -0,0 +1,10 @@
+package com.xdx.textviewdemo.utils;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2018/7/29.
+ */
+
+public class TestBActivity extends AppCompatActivity {
+}
